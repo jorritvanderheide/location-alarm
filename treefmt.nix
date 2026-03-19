f10188f380534da7ae9fde649cb3e7ab084f4332
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-03-19 08:34:00 +0100

    refactor: formatting

--- treefmt.nix
+++ treefmt.nix
@@ -3,10 +3,4 @@
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
   programs.dart-format.enable = true;
-  programs.yamlfmt.enable = true;
-
-  settings.formatter.yamlfmt.excludes = [
-    "pubspec.yaml"
-    "pubspec.lock"
-  ];
 }
